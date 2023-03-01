379ac629a049bbe15e8d51951eac8d5bafdf8796
hessa54 <hessafahad54@.gmail.com>
2023-03-01 20:17:32 +0300

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,80 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("Journaly"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello World!'),
+              Text('Welcome back'),
+              Container(
+                height: 30,
+                width: 100,
+                child: Center(child: Text('Username')),
+                color: Colors.white,
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 30,
+                width: 100,
+                child: Center(child: Text('Password')),
+                color: Colors.white,
+              ),
+              Text('Reset password'),
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () => {print("Sign in")},
+                  child: Text("Sign in")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('----'),
+                  Text('Continue with'),
+                  Text('----'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () => {print("Sign in")},
+                    child: Text('Google'),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                    onPressed: () => {print("Sign in")},
+                    child: Text('Apple'),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: () => {print("Sign in")},
+                    child: Text('FaceBook'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('do you have an account ?'),
+                  Text('Create new account'),
+                ],
+              )
+            ],
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 197, 251, 200),
       ),
     );
   }
