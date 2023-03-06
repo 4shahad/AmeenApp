78d34deffc3d051e2c2edd31f748e9536deb9f21
hessa54 <hessafahad54@.gmail.com>
2023-03-06 17:58:58 +0300

    navigater to id

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,68 @@
-
-
 import 'package:flutter/material.dart';
 import 'intro.dart';
 
-
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('asset/images/logo.png'),
-              Intro(),
-            ],
-          ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('asset/images/logo.png'),
+            Intro(),
+            ElevatedButton(
+              child: const Text('رقم الهوية'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondRoute()),
+                );
+              },
+            ),
+          ],
         ),
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
       ),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('رقم الهوية'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 72, 157, 142),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('asset/images/id.jpeg'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('تحقق'),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
     );
   }
 }
